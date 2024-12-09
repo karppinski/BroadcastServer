859ced0279b2bb1bb3e458c2f2a3f824f18a62b9
karppinski <krzysztof.karpinski135@gmail.com>
2024-12-09 23:13:22 +0100

    First base server and client is ready

---
+++ BroadcastClient/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Concurrent;
+using System.Net.WebSockets;
+using System.Text;
+
+namespace BroadcastClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var clientWebSocket = new ClientWebSocket();
+
+            try
+            {
+                await clientWebSocket.ConnectAsync(new Uri("ws://localhost:7000/"), CancellationToken.None);
+
+                while (clientWebSocket.State == WebSocketState.Open)
+                {
+                    Console.WriteLine("Write message");
+                    string message = Console.ReadLine();
+
+                    if(message == "exit")
+                    {
+                        Console.WriteLine("Closing connection");
+                        await clientWebSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "", CancellationToken.None);
+                    }
+
+                    var messageToSend = Encoding.UTF8.GetBytes(message);
+
+                    await clientWebSocket.SendAsync
+                        (new ArraySegment<byte>(messageToSend)
+                        , WebSocketMessageType.Text,
+                        true,
+                        CancellationToken.None);
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"There is a problem {ex}");
+            }
+        }
+    }
+}
--- BroadcastServer/Program.cs
+++ BroadcastServer/Program.cs
@@ -1,10 +1,62 @@
-﻿namespace BroadcastServer
+﻿using System.Diagnostics;
+using System.Net;
+using System.Net.WebSockets;
+using System.Text;
+
+namespace BroadcastServer
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
+        static async Task Main()
         {
-            Console.WriteLine("Hello, World!");
+            var httpListener = new HttpListener();
+            httpListener.Prefixes.Add("http://localhost:7000/");
+            httpListener.Start();
+            Console.WriteLine("Server set and waiting for connections!");
+
+            while (true)
+            {
+                var  context = await httpListener.GetContextAsync();
+                if(context.Request.IsWebSocketRequest)
+                {
+                    await ProcessRequest(context);
+                }
+                else
+                {
+                    context.Response.StatusCode = 400;
+                    context.Response.Close();
+                }
+            }
+        }
+
+        static async Task ProcessRequest(HttpListenerContext context)
+        {
+            WebSocketContext webSocketContext = await context.AcceptWebSocketAsync(null);
+            var webSocket = webSocketContext.WebSocket;
+            Console.WriteLine("Client connected");
+
+            var buffer = new byte[1024];
+
+            while (webSocket.State == WebSocketState.Open)
+            {
+                var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+                if(result.MessageType == WebSocketMessageType.Text)
+                {
+                    var receivedMessage = Encoding.UTF8.GetString(buffer, 0, result.Count);
+                    Console.WriteLine($"Reviced {receivedMessage}");
+
+                    string responseMessage = $"Server received message {receivedMessage}";
+                    var responseBuffer = Encoding.UTF8.GetBytes(responseMessage);
+                    await webSocket.SendAsync(responseBuffer, WebSocketMessageType.Text, true, CancellationToken.None);
+                }
+                else if(result.MessageType == WebSocketMessageType.Close)
+                {
+                    Console.WriteLine("Client disconected.");
+                    await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "", CancellationToken.None);
+                    break;
+                }
+            }
         }
     }
 }
